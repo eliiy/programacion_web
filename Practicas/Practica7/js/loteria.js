7726d4f601be0af72bbd9ea2ab54c43a1f0ce50f
13030605 Elizabeth <elizabeth-perez-m@hotmail.com>
2017-03-10 20:05:45 -0600

    Practica7

--- Practicas/Practica7/js/loteria.js
+++ Practicas/Practica7/js/loteria.js
@@ -1,15 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
 	console.log("Hola desde jquery");
-	//alert("hola desde jquery");
-	$("img").click(
-		function(){
-			//$(this).width("20px");
-			//$(this).hide("slow");
-			//$(this).show("fast");
-			$(this).attr('src','imagen/2.jpg');
-	});
+	$("#mensaje-ganador").hide();
+	$("#mensaje-perdedor").hide();
+	var arreglo1 = ['2','3','4','5','6','7','8','9','10'];
+	var arreglo2 = [];
+	var con=0;
+	var imagen=0;
+	var tiempo;
+	var i,j,k;
+    for (i = arreglo1.length; i; i--) 
+    {
+        j = Math.floor(Math.random() * i);
+        k = arreglo1[i - 1];
+        arreglo1[i - 1] = arreglo1[j];
+        arreglo1[j] = k;
+        arreglo2.push(arreglo1[i-1]);
+    }
+
+	 tiempo = setInterval(
+		function()
+		{
+			console.log(arreglo2[con]);
+			$("#"+arreglo2[con]).click(
+				function ()
+				{
+						//$(this).width("20px");
+						//$(this).hide("slow");
+						//$(this).show("fast");
+					$(this).attr('src','imagen/frijol.png');
+					imagen=imagen+1;
+					if(imagen==9 && con==9)
+					{
+						$("#mensaje-ganador").show();
+						clearInterval(tiempo);
+					}
+				});
+			
+			$("#1").attr('src','imagen/'+arreglo2[con]+'.jpg');
+			con=con+1;
+			if(con==10 && imagen<9)
+			{
+				$("#1").attr('src','imagen/perder.jpg');
+				clearInterval(tiempo);
+				$("#mensaje-perdedor").show();
+			}
+		}, 3000);
+			
+		
+	
+	
 	$("#btn-reiniciar").click(
-		function(){
-			$("img").show("fast");
-	});
+		function()
+		{
+			
+			//$("#img").show();
+		});
+
+	
 });
